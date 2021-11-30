8487050bbfd71108215a89742686567c0a4bbb8d
Kaylee <06.kaylee@gmail.com>
2021-11-30 09:37:10 -0700

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="container">
+    <NavBar />
+    <router-view/>
   </div>
-  <router-view/>
 </template>
+
+<script>
+import NavBar from './components/NavBar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
@@ -15,16 +25,4 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
