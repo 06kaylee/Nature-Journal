2963cabdefa5cc33922b3b14a3024f52df3c8730
Kaylee <06.kaylee@gmail.com>
2021-12-02 23:53:22 -0700

    adds log controller

---
+++ server/controllers/logController.js
@@ -0,0 +1,26 @@
+const Log = require("../models/log");
+
+// create and save a new log
+exports.createLog = (req, res) => {
+
+}
+
+// get all logs
+exports.allLogs = (req, res) => {
+
+}
+
+// get a single log by id
+exports.oneLog = (req, res) => {
+
+}
+
+// update a single log by id
+exports.updateLog = (req, res) => {
+
+}
+
+// delete a single log by id
+exports.deleteLog = (req, res) => {
+    
+}
